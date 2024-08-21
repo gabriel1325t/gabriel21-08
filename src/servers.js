81401fbda87bbdabf852f5cf6f626a0396e8fea7
gabriel1325t <gabriel1325@gmail.com>
2024-08-21 11:12:52 -0300

    first commit

---
+++ src/servers.js
@@ -0,0 +1,4 @@
+// Importar a instância da Express configurada em idex.js
+const app = require("./index");
+//Inica o servidor na porta 3000, neste caso a API será acessiva em http://localhost:3000/
+app.listen(3000);
